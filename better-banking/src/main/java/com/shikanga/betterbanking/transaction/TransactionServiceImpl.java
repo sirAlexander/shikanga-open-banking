caece69fa03f89f8cda4bfe9d3d7084cb4248c07
sir'Alexander <alexander.shikanga.tindi@gmail.com>
2022-03-11 22:49:41 +0000

    removed money import, added dummy transaction

--- better-banking/src/main/java/com/shikanga/betterbanking/transaction/TransactionServiceImpl.java
+++ better-banking/src/main/java/com/shikanga/betterbanking/transaction/TransactionServiceImpl.java
@@ -1,5 +1,6 @@
 package com.shikanga.betterbanking.transaction;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -16,7 +17,10 @@ public class TransactionServiceImpl implements TransactionService {
                         .date(LocalDateTime.now())
                         .accountNumber(accountNumber)
                         .currency("USD")
-                           .build()
+                        .amount(BigDecimal.valueOf(100.00))
+                        .merchantName("acme")
+                        .merchantLogo("images/acme-logo.png")
+                        .build()
         );
     }
 }
