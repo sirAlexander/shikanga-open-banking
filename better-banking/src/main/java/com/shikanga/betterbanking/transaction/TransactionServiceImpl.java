1eec24566c308b64e832879d88c5275d95efb9e0
Alexander Shikanga-Tindi <alexander.tindi@yapily.com>
2022-03-11 22:23:11 +0000

    added moneta dependency to support Java Money

--- better-banking/src/main/java/com/shikanga/betterbanking/transaction/TransactionServiceImpl.java
+++ better-banking/src/main/java/com/shikanga/betterbanking/transaction/TransactionServiceImpl.java
@@ -1,11 +1,22 @@
 package com.shikanga.betterbanking.transaction;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class TransactionServiceImpl implements TransactionService {
 
     @Override
     public List<Transaction> findAllByAccountNumber(final Integer accountNumber) {
-        return null;
+        return List.of(
+                Transaction.builder()
+                        .type("credit")
+                        .date(LocalDateTime.now())
+                        .accountNumber(accountNumber)
+                        .currency("USD")
+                           .build()
+        );
     }
 }
