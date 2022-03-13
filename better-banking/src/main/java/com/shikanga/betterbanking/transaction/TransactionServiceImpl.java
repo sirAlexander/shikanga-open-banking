4049ab3d84f55c056d7c957d599c4e32bed1f568
sirAlexander <alexander.shikanga.tindi@gmail.com>
2022-03-13 20:43:56 +0000

    updated transaction service to use new jps repository

--- better-banking/src/main/java/com/shikanga/betterbanking/transaction/TransactionServiceImpl.java
+++ better-banking/src/main/java/com/shikanga/betterbanking/transaction/TransactionServiceImpl.java
@@ -1,26 +1,18 @@
 package com.shikanga.betterbanking.transaction;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class TransactionServiceImpl implements TransactionService {
+
+    private final TransactionRepository transactionRepository;
 
     @Override
     public List<Transaction> findAllByAccountNumber(final Integer accountNumber) {
-        return List.of(
-                Transaction.builder()
-                        .type("credit")
-                        .date(LocalDateTime.now())
-                        .accountNumber(accountNumber)
-                        .currency("USD")
-                        .amount(BigDecimal.valueOf(100.00))
-                        .merchantName("acme")
-                        .merchantLogo("images/acme-logo.png")
-                        .build()
-        );
+        return transactionRepository.findAllByAccountNumber(accountNumber);
     }
 }
